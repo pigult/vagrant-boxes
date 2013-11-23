706ef770cde4f7236a552cb25bf0cfa502b0d2eb
donis <donis+github@gildija.lt>
2013-11-23 20:41:30 +0200

    - Removing vmware and chef  - cleaning up postfix from default system

--- definitions/CentOS-6.4-x86_64/definition.rb
+++ definitions/CentOS-6.4-x86_64/definition.rb
@@ -26,11 +26,9 @@ Veewee::Session.declare({
   :shutdown_cmd => "/sbin/halt -h -p",
   :postinstall_files => [
     "base.sh",
-    "chef.sh",
     "puppet.sh",
     "vagrant.sh",
     "virtualbox.sh",
-    #"vmfusion.sh",
     "cleanup.sh",
     "zerodisk.sh"
   ],
